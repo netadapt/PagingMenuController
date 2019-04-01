6626d116679782d126b134ac382e18d5d6e83c93
netadapt <russ@netadapt.com>
2019-04-01 16:00:43 +0100

    Update MenuViewCustomizable.swift

--- Pod/Classes/Protocols/MenuViewCustomizable.swift
+++ Pod/Classes/Protocols/MenuViewCustomizable.swift
@@ -37,7 +37,7 @@ public extension MenuViewCustomizable {
         return 0.3
     }
     var deceleratingRate: CGFloat {
-        return UIScrollViewDecelerationRateFast
+        return UIScrollView.DecelerationRate.fast; //UIScrollViewDecelerationRateFast
     }
     var selectedItemCenter: Bool {
         return true
